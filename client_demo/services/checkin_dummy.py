6de6186f5b9061eae4378f1f3a0aafff635768d9
Abhay Sajeev <abhaysajeev58@gmail.com>
2025-08-29 10:53:57 +0530

    Initial commit - Custom Frappe App

---
+++ client_demo/services/checkin_dummy.py
@@ -0,0 +1,39 @@
+# File: your_app/your_app/api/attendance.py
+import frappe
+from frappe import _
+import datetime 
+@frappe.whitelist(allow_guest=True)
+def mark_attendance(employee, log_type=None, device_id=None, shift=None):
+    """
+    Insert an attendance record into Employee Checkin.
+    Required: employee, log_type, time
+    Optional: device_id, shift
+
+    """
+    # Validate employee exists
+    if not frappe.db.exists("Employee", employee):
+        return {"success": False, "message": _(f"Employee {employee} not found")}
+
+    # # Validate log_type
+    # if log_type not in ["IN", "OUT"]:
+    #     return {"success": False, "message": _("Invalid log_type, must be IN or OUT")}
+
+    # Create Employee Checkin record
+
+    time = datetime.datetime.now()
+    
+    try:
+        checkin = frappe.get_doc({
+            "doctype": "Employee Checkin",
+            "employee": employee,
+            "log_type": log_type,
+            "time": time,
+            "device_id": device_id,
+            "shift": shift
+        })
+        checkin.insert(ignore_permissions=True)  # allow guest insert
+        frappe.db.commit()
+        return {"success": True, "message": "Attendance recorded successfully"}
+    except Exception as e:
+        return {"success": False, "message": str(e)}
+
